926bafb1bda8089b38aa5c1030e6fe9309c0c010
Andrea Mayor <113984748+andreamgom@users.noreply.github.com>
2026-04-19 13:46:41 +0100

    Add ascending and descending order parameter (#3)
    Co-authored-by: Carlos Alonso <carlos.alonso511@comunidadunir.net>

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import sys
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
+DEFAULT_ASCENDING = True
 
 
 def sort_list(items, ascending=True):
@@ -24,9 +25,12 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    ascending = DEFAULT_ASCENDING
+
+    if len(sys.argv) == 4:
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
+        ascending = sys.argv[3].lower() == "asc"
     else:
         print("The file must be indicated as the first argument")
         print("The second argument indicates if duplicates should be removed")
@@ -46,4 +50,4 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    print(sort_list(word_list, ascending=ascending))
